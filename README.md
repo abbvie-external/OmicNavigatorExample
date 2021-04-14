3cb6ac5ec90f31b5471ced7070ca48c56f1cde39
John Blischak <john.blischak@abbvie.com>
2021-04-14 09:14:04 -0400

    Add script to install packages.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # How to create an OmicNavigator study
+
+This repository contains an example of how to convert an omic analysis into a
+study package to be explored with the OmicNavigator app.
+
+**Files:**
+
+* `setup.R` - Installs the required R packages for the example
---
+++ setup.R
@@ -0,0 +1,10 @@
+# Installs the required packages from CRAN, Bioconductor, and GitHub.
+
+cran <- c("BiocManager", "ggplot2", "remotes")
+install.packages(cran)
+
+bioc <- c("edgeR", "limma")
+BiocManager::install(bioc, update = FALSE)
+
+github <- c("abbvie-external/OmicNavigator@*release")
+remotes::install_github(github, dependencies = TRUE, upgrade = FALSE)
