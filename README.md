4eb8394626ebdba578a405a1e7d08d2e7349393e
John Blischak <john.blischak@abbvie.com>
2021-04-15 07:08:43 -0400

    Export analysis results.

--- README.md
+++ README.md
@@ -7,10 +7,17 @@ study package to be explored with the OmicNavigator app.
 
 * [`setup.R`](./setup.R) - Installs the required R packages for the example
 
+* [`data/`](./data/) - The input RNA-seq counts and MSigDB annotations
+
 * [`analyze.R`](./analyze.R) - Performs a differential expression and enrichment
-analysis of an RNA-seq experiment. It uses limma+voom, but OmicNavigator is
-agnostic to how you perform your analysis. If you prefer, you could use Python
-or a GUI to perform the analysis, as long as you export the results.
+analysis of the RNA-seq experiment in [`data/`](./data/). It uses limma+voom,
+but OmicNavigator is agnostic to how you perform your analysis. If you prefer,
+you could use Python or a GUI to perform the analysis, as long as you export the
+results.
+
+* [`results/`](./results/) - The analysis results exported by
+[`analyze.R`](./analyze.R)
 
 * [`build.R`](./build.R) - Builds the OmicNavigator study package from the
-results of `analyze.R`
+results files that [`analyze.R`](./analyze.R) exported to
+[`results/`](./results/)
--- analyze.R
+++ analyze.R
@@ -1,14 +1,14 @@
 # Analyze RNA-seq experiment with limma+voom
 
+# Prepare data -----------------------------------------------------------------
 
 # Load required packages
 library("limma")
 library("edgeR")
-#library("ggplot2")
 library("Mus.musculus")
 
 # Import RNA-seq counts
-files <- Sys.glob("input/*txt")
+files <- Sys.glob("data/*txt")
 x <- readDGE(files, columns = c(1, 3))
 
 # Organize sample data
@@ -48,28 +48,58 @@ keep.exprs <- rowSums(cpm > 1) >= 3
 x <- x[keep.exprs, , keep.lib.sizes = FALSE]
 x <- calcNormFactors(x, method = "TMM")
 
-# Differential expression analysis
+# Differential expression analysis ---------------------------------------------
+
+# Specify linear model and contrasts to test
 design <- model.matrix(~0 + group + lane, data = x$samples)
 colnames(design) <- sub("(group|lane)", "", colnames(design))
 contrastsMatrix <- makeContrasts(BasalvsLP = Basal - LP,
                                  BasalvsML = Basal - ML,
                                  LPvsML = LP - ML,
                                  levels = colnames(design))
+
+# Fit the model
 v <- voom(x, design)
 vfit <- lmFit(v, design)
 vfit <- contrasts.fit(vfit, contrasts = contrastsMatrix)
 efit <- eBayes(vfit)
 
-# Enrichment analysis
+# Enrichment analysis ----------------------------------------------------------
 
 # Load Mm.h, which contains the MSigDB Hallmark gene sets for the enrichment
 # analysis
-load("input/mouse_v5p2.rdata")
-
+load("data/mouse_H_v5p2.rdata")
 idx <- ids2indices(Mm.H, id = rownames(v))
+
+# Calculate enrichment for each of the 3 tests
 enrichedBasalvsLP <- camera(v, idx, design, contrast = contrastsMatrix[, 1])
 enrichedBasalvsML <- camera(v, idx, design, contrast = contrastsMatrix[, 2])
 enrichedLPvsML <- camera(v, idx, design, contrast = contrastsMatrix[, 3])
 
-# Export results
-dir.create("output", showWarnings = FALSE)
+# Export results ---------------------------------------------------------------
+
+dir.create("results", showWarnings = FALSE)
+export <- function(x, file, row.names = FALSE, ...) {
+  write.table(x, file = file, quote = FALSE, sep = "\t", row.names = row.names, ...)
+}
+
+export(x$samples, "results/samples.txt")
+export(x$genes, "results/features.txt")
+export(metaFeatures, "results/metaFeatures.txt")
+
+# Convert counts to log-counts-per-million
+assays <- as.data.frame(cpm(x, log = TRUE))
+export(assays, "results/assays.txt", row.names = TRUE)
+
+# Test results
+create_results_table <- function(fit, coef) {
+  topTable(fit, coef = coef, number = Inf, sort.by = "p")[, -2:-3]
+}
+export(create_results_table(efit, "BasalvsLP"), "results/BasalvsLP.txt")
+export(create_results_table(efit, "BasalvsML"), "results/BasalvsML.txt")
+export(create_results_table(efit, "LPvsML"), "results/LPvsML.txt")
+
+# Enrichments
+export(enrichedBasalvsLP, "results/enrichedBasalvsLP.txt", row.names = TRUE)
+export(enrichedBasalvsML, "results/enrichedBasalvsML.txt", row.names = TRUE)
+export(enrichedLPvsML, "results/enrichedLPvsML.txt", row.names = TRUE)
